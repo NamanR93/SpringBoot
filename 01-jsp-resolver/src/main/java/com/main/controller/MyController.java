f1e60bf4d2c84ac9fc01b920fe9ecf63151f0219
Naman Rawat <namanrawat90.nr@gmail.com>
2025-11-24 20:43:43 +0530

    about JSP

---
+++ 01-jsp-resolver/src/main/java/com/main/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/home")
+	public String hello() {
+		return "hello world";
+	}
+
+}
