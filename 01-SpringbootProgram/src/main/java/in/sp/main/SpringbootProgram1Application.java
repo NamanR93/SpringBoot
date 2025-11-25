c916629ed7eb8c296c69f7f74663306fb5e06356
Naman Rawat <namanrawat90.nr@gmail.com>
2025-11-25 16:09:10 +0530

    program-1

---
+++ 01-SpringbootProgram/src/main/java/in/sp/main/SpringbootProgram1Application.java
@@ -0,0 +1,15 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProgram1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProgram1Application.class, args);
+		
+		System.out.println("This is demo Springboot program");
+	}
+
+}
